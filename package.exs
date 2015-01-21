de849cb2c01165068759e9e4229496a1ec2c57f4
Duncan M. McGreggor <oubiwann@cogitat.io>
2015-01-21 00:10:31 -0600

    Bumped to version 0.3.0.

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(
   name: "kla",
   description: "An LFE Wrapper Library used to Dress Up Erlang Libraries in a Lispy Costume",
-  version: "0.0.1",
+  version: "0.3.0",
   keywords: ["LFE", "Lisp", "Library", "API"],
   maintainers: [[name: "Bill Ota", email: "bill.ota@billo.systems"]],
   repositories: [[github: "billosys/kla"]])
